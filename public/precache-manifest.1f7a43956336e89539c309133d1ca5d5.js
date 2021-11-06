36dfd729925542fbb1ef09fc5933d51e1df66459
uyuer <13777072927@163.com>
2021-11-06 18:02:34 +0800

    feat:添加静态文件

---
+++ public/precache-manifest.1f7a43956336e89539c309133d1ca5d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91f677d701c61a8caef2d4fa07d6bd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e0e6b4ec3ed45780902",
+    "url": "/static/css/2.8a8a0ba8.chunk.css"
+  },
+  {
+    "revision": "5ffd4be6ac76b44ae7bd",
+    "url": "/static/css/main.e027272c.chunk.css"
+  },
+  {
+    "revision": "0e0e6b4ec3ed45780902",
+    "url": "/static/js/2.8f645096.chunk.js"
+  },
+  {
+    "revision": "0a2326fd2ca1cd67a1fb4ace74afbe74",
+    "url": "/static/js/2.8f645096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ffd4be6ac76b44ae7bd",
+    "url": "/static/js/main.c4a0337c.chunk.js"
+  },
+  {
+    "revision": "17bab6d9cf19d4f6fe7b",
+    "url": "/static/js/runtime-main.f0fc498d.js"
+  },
+  {
+    "revision": "45836833440e86732281a72c7a096ac8",
+    "url": "/static/media/avatar.45836833.jpg"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.06a861c2dfa63c07a346a9a9998dd7eb.js"
+  "/precache-manifest.1f7a43956336e89539c309133d1ca5d5.js"
 );
 
 self.addEventListener('message', (event) => {
